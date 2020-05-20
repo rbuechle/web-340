d4cf8b31275e619c474274a2f7d19143d2bf038f
Becca <rbuechle@my365.bellevue.edu>
2020-05-20 17:39:14 -0500

    4.2 – JSON APIs

---
+++ week-4/json-api/buechle_4.2.js
@@ -0,0 +1,31 @@
+/*
+============================================
+; Title: Assignment 4.2 JSON APIs
+; Author: Professor Itskovich
+; Date: 20 May 2020
+; Modified By: Becca Buechle
+; Description: 4.2 JSON APIs 
+;===========================================
+*/
+
+
+var express = require("express");
+
+var http = require("http");
+
+var app = express();
+
+app.get("/customer/:id", function (request, response){
+    
+    var id = parseInt(request.params.id, 10);
+    
+    response.json({
+        firstName: "Becca",
+        lastName: "Buechle",
+        employeeId: 1234
+    });
+});
+//starts server on port 8080
+http.createServer(app).listen(8080, function(){
+    console.log("Application started on port 8080");
+});
