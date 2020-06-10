22ebf0f4f0396e86fc91f5ec5bc65af9a3024720
Becca <rbuechle@my365.bellevue.edu>
2020-06-10 01:03:09 -0500

    adding employee and mongoose

--- ems/app.js
+++ ems/app.js
@@ -1,19 +1,29 @@
 var express = require("express");
-
 var http = require("http");
-
+var mongoose = require("mongoose");
 var path = require("path");
-
 var logger = require("morgan");
-
 var app = express();
+var Employee = require('./models/employee');
+
+// database connection string to MongoDB 
+var mongoDB = "mongodb+srv://rbuechle:Seattle06@cluster0-japve.mongodb.net/test?retryWrites=true&w=majority"
+mongoose.connect(mongoDB,{
+    useMongoClient: true
+});
+
+mongoose.Promise = global.Promise;
+
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error: "));
+db.once("open", function(){
+    console.log("Application connected to MongoDB Atlas")
+});
+
 
 app.set("views", path.resolve(__dirname, "views"));
-
 app.set("view engine", "ejs");
-
 app.use(logger("short"));
-
 app.use(express.static(path.join(__dirname + '/public')))
 
 app.get("/", function (req, res){
@@ -22,6 +32,13 @@ app.get("/", function (req, res){
     });
 });
 
+//start server
 http.createServer(app).listen(8080, function(){
     console.log("Application started on port 8080");
+});
+
+// model
+var employee = new Employee({
+    firstName: "Becca",
+    lastName: "Buechle"
 });
---
+++ ems/models/employee.js
@@ -0,0 +1,12 @@
+// required
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+// define Employee Schema
+var EmployeeSchema = new Schema({
+    firstName: {type: String, required: true },
+    lastName: {type: String, required: true}
+});
+
+// Export model 
+module.exports = mongoose.model('Employee', EmployeeSchema);
